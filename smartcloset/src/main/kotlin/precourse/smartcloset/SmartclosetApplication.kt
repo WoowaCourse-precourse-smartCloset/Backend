63d77242230b915951f13fd69c810387970e935e
hyeonjune <annhj980@naver.com>
2025-11-05 01:17:26 +0900

    feat: setup project

---
+++ smartcloset/src/main/kotlin/precourse/smartcloset/SmartclosetApplication.kt
@@ -0,0 +1,11 @@
+package precourse.smartcloset
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartclosetApplication
+
+fun main(args: Array<String>) {
+    runApplication<SmartclosetApplication>(*args)
+}
---
+++ smartcloset/src/test/kotlin/precourse/smartcloset/SmartclosetApplicationTests.kt
@@ -0,0 +1,13 @@
+package precourse.smartcloset
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmartclosetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
