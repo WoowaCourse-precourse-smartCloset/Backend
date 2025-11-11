2c752e1cfb3508c767e9afe7297fc44234ea6f38
hyeonjune <annhj980@naver.com>
2025-11-12 04:57:13 +0900

    refactor: 엔티티 필드 삭제에 따라 Response DTO 정리

--- smartcloset/src/main/kotlin/precourse/smartcloset/user/dto/RegisterResponse.kt
+++ smartcloset/src/main/kotlin/precourse/smartcloset/user/dto/RegisterResponse.kt
@@ -7,15 +7,13 @@ data class RegisterResponse (
     val userId: Long,
     val email: String,
     val nickname: String,
-    val createdAt: LocalDateTime
 ){
     companion object {
         fun from(user: User): RegisterResponse {
             return RegisterResponse(
                 userId = user.id!!,
                 email = user.email,
-                nickname = user.nickname,
-                createdAt = user.createdAt
+                nickname = user.nickname
             )
         }
     }
